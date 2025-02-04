de33ebfd3955adc707d967bd111e3a5b0804fe9e
DariaPuhach <142429450+DariaPuhach@users.noreply.github.com>
2025-02-04 20:39:49 +0100

    add hw1

---
+++ hw-1/main.js
@@ -0,0 +1,54 @@
+// let a="hello";
+// console.log(a);
+// let b='owu';
+// console.log(b);
+// let c='com';
+// console.log(c);
+// let d='ua';
+// console.log(d);
+// let e=1;
+// console.log(e);
+// let f=10;
+// console.log(f);
+// let g=123;
+// console.log(g);
+// let h=-999;
+// console.log(h);
+// let i=3.14;
+// console.log(i);
+// let j=2.7;
+// console.log(j);
+// let k=16;
+// console.log(k);
+// let l=true;
+// console.log(l);
+// let m=false;
+// console.log(m);
+// ------------------------------------------------
+
+// let firstName = "Дар'я";
+// let middleName = "Дмитрівна";
+// let lastName = "Пугач";
+//
+// let person = firstName + " " + middleName + " " + lastName;
+// console.log(person);
+
+//--------------------------------------------------
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// ------------------------------------------
+
+// let firstName = prompt("Введіть ваше ім'я:");
+// let middleName = prompt("Введіть ваше по-батькові:");
+// let age = prompt("Введіть ваш вік:");
+//
+// console.log("Ім'я: " + firstName);
+// console.log("По-батькові: " + middleName);
+// console.log("Вік: " + age);
